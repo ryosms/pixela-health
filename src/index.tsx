a4b7d4a0769ef9b2d93ed8264fde3cb715217f63
ryosms <ryo.sms@gmail.com>
2020-07-16 12:10:06 +0900

    Add global header

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App}/>
-    </Switch>
-  </BrowserRouter>,
+  <React.Fragment>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App}/>
+      </Switch>
+    </BrowserRouter>
+    <CssBaseline/>
+    <AppBar position="absolute">
+      <Toolbar>
+        <Typography variant="h6" color="inherit" noWrap>
+          Pixela Health
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  </React.Fragment>,
   document.getElementById('root')
 );
 
